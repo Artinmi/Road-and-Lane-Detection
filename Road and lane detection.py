313ef2603303e401af3bf34ea52e8f9b486d8f02
Artin Mokhtariha <147430167+Artinmi@users.noreply.github.com>
2025-03-23 23:46:57 +0330

    Update Road and lane detection.py

--- Road and lane detection.py
+++ Road and lane detection.py
@@ -4,7 +4,7 @@ import os
 from scipy.misc import imresize
 from moviepy.editor import VideoFileClip
 from tensorflow import keras
-1
+
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the path to the model file relative to the script's directory
